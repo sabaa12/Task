07d61829106d16f450eafeb1dbab838fececa66e
sabal <saba.liklikadze1@gmail.com>
2023-03-13 22:37:47 +0400

    fisrt

---
+++ Client/Program.cs
@@ -0,0 +1,143 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.Reflection;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Newtonsoft.Json;
+using Server.Protos;
+
+namespace FileListener
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            using var watcher = new FileSystemWatcher(@"D:\Test");
+
+            watcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+
+            watcher.Created += OnCreated;
+            watcher.Error += OnError;
+
+            watcher.Filter = "*.json";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+
+          
+
+        }
+        private static readonly object _lockObject = new object();
+
+        private static void OnCreated(object source, FileSystemEventArgs e)
+        {
+            var path = e.FullPath;
+            List<dynamic> batch = new List<dynamic>();
+            while (IsFileLocked(e.FullPath))
+            {
+                Thread.Sleep(500);
+            }
+
+            lock (_lockObject)
+            {
+
+                using (var fileStream = new FileStream(path, FileMode.Open))
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    string line;
+
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        var jsonReader = new JsonTextReader(new StringReader(line))
+                        {
+                            SupportMultipleContent = true
+                        };
+                        var jsonSerializer = new JsonSerializer();
+                        batch.Add(System.Text.Json.JsonSerializer.Deserialize<dynamic>(line));
+
+                    }
+
+
+                }
+                string jsonBatch = System.Text.Json.JsonSerializer.Serialize(batch);
+
+                try
+                {
+                    SendRequest(jsonBatch,batch);
+                    File.Delete(e.FullPath);
+
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("error occured"); ;
+                }
+
+            }
+
+
+
+        }
+
+        private static async Task SendRequest(string jsonBatch, List<dynamic> batch)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5042");
+           // var client = new GrpcService.GrpcServiceClient(channel);
+            var client=new GrpcService.GrpcServiceClient(channel);
+            
+            // create a google.protobuf.Any object and pack the DynamicMessage into it
+             using (var call = client.GetInfo(new GrpcRequest { Payload = ConvertListToAny(batch) }))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    Console.WriteLine($"this type was successfully sent {call.ResponseStream.Current.Type}");
+                }
+            }
+
+        }
+        public static Any ConvertListToAny<T>(List<T> list)
+        {
+            // Serialize the list to a byte array using System.Text.Json.
+            byte[] bytes = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(list);
+
+            // Create a new Any object and set its properties.
+            Any any = new Any();
+            any.Value = ByteString.CopyFrom(bytes);
+            any.TypeUrl = $"type.googleapis.com/{typeof(List<T>).FullName}";
+
+            return any;
+        }
+        private static void OnError(object source, ErrorEventArgs e)
+        {
+            Console.WriteLine($"FileSystemWatcher error: {e.GetException().Message}");
+        }
+        private static bool IsFileLocked(string filePath)
+        {
+            try
+            {
+                using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+
+                }
+            }
+            catch (IOException)
+            {
+
+                return true;
+            }
+
+
+            return false;
+        }
+    }
+}
---
+++ GrpcServer/Services/Service.cs
@@ -0,0 +1,87 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Server.Protos;
+using System.Text;
+
+namespace GrpcServer.Services
+{
+    public class Service : GrpcService.GrpcServiceBase
+    {
+        public override async Task GetInfo(GrpcRequest request, IServerStreamWriter<GrpcResponse> responseStream, ServerCallContext context)
+        {
+
+            List<dynamic> logs = ConvertAnyToList(request.Payload);
+            var groups = logs.GroupBy(log => (string)log.type);
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string rootDirectory = Path.Combine(currentDirectory, "root");
+
+            await WriteTofile(responseStream, groups, rootDirectory);
+
+        }
+
+        private static async Task WriteTofile(IServerStreamWriter<GrpcResponse> responseStream, IEnumerable<IGrouping<string, dynamic>> groups, string rootDirectory)
+        {
+            string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+
+            foreach (var group in groups)
+            {
+                string typeDirectory = Path.Combine(rootDirectory, group.Key);
+
+                if (!Directory.Exists(typeDirectory))
+                {
+                    Directory.CreateDirectory(typeDirectory);
+                }
+
+                string filePath = Path.Combine(typeDirectory, currentDate + ".json").Replace('\\', '/');
+
+                try
+                {
+                    string Json = "";
+                    if (File.Exists(filePath))
+                    {
+                        var json = File.ReadAllText(filePath);
+                        var list = JsonConvert.DeserializeObject<List<dynamic>>(json);
+
+                        if (list != null)
+                        {
+                            list.AddRange(group);
+                        }
+                        else
+                        {
+                            list = new List<dynamic> { JToken.Parse(json), group };
+                        }
+
+                        Json = JsonConvert.SerializeObject(list, Formatting.Indented);
+                    }
+                    else
+                    {
+                        var jsonArray = JArray.FromObject(group);
+                        Json = jsonArray.ToString(Formatting.Indented);
+
+                    }
+
+                    File.WriteAllText(filePath, Json);
+                    await responseStream.WriteAsync(new GrpcResponse { Type = group.Key });
+
+
+                }
+                catch (Exception)
+                {
+                    await responseStream.WriteAsync(new GrpcResponse { Type = string.Empty });
+                }
+            }
+        }
+        public static List<dynamic> ConvertAnyToList(Any any)
+        {
+            var bytes = any.Value.ToByteArray();
+
+            List<dynamic> list = JsonConvert.DeserializeObject<List<dynamic>>(Encoding.ASCII.GetString(bytes));
+
+            return list;
+        }
+
+    }
+}
